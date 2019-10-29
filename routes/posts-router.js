f0c68d247da3988e3da22d7881e4d89f4484a0b6
Devin Warrick <dev.warr@gmail.com>
2019-10-29 13:10:50 -0500

    single post PUT request functions properly

--- routes/posts-router.js
+++ routes/posts-router.js
@@ -31,11 +31,51 @@ router.post("/", (req, res) => {
 
 // endpoint: /:id
 router.get("/:id", (req, res) => {
-    
-})
+    if (!Number(req.params.id)) {
+        res.status(400).json({errorMessage:"Looks like you didn't give a number!", details:"Where you see ':id' -> That should be a number, like '/23'."});
+        return;
+    }
+
+    db.findById(req.params.id)
+        .then(post => {
+            if (!post) {
+                req.status(404).json({errorMessage:"We couldn't find a post with that id. Sorry! >.>"});
+                return;
+            }
+            res.status(200).json(post);
+        })
+        .catch(err => res.status(500).json({error: "We couldn't add the posts into the database. Internal error x_x"}))
+});
 
 router.put("/:id", (req, res) => {
-    
+    if (!Number(req.params.id)) {
+        res.status(400).json({errorMessage:"Looks like you didn't give a number!", details:"Where you see ':id' -> That should be a number, like '/23'."});
+        return;
+    }
+    if (!req.body.title && !req.body.contents) {
+        res.status(400).json({errorMessage:"Oops! You need at least a 'title' or 'contents' when you update a post!"});
+        return;
+    } 
+
+    const changes = {};
+    if (req.body.title) {
+        changes.title = req.body.title;
+    }
+    if (req.body.contents) {
+        changes.contents = req.body.contents;
+    }
+
+    db.update(req.params.id, changes)
+        .then(count => {
+            if(!count) {
+                res.status(404).json({errorMessage:"We couldn't find a post with that id o.o"});
+                return;
+            }
+            db.findById(req.params.id)
+                .then(post => res.status(200).json(post));
+        })
+        .catch(err => res.status(500).json({error: "We couldn't add the posts into the database. Internal error x_x"}))
+
 })
 
 router.delete("/:id", (req, res) => {
