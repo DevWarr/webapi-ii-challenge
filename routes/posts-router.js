8da2757529242faa66e7c525bf4d99d0b6b7d4b5
Devin Warrick <dev.warr@gmail.com>
2019-10-29 12:45:13 -0500

    Created empty methods for each endpoint

--- routes/posts-router.js
+++ routes/posts-router.js
@@ -1,10 +1,41 @@
 const db = require("../data/db");
 const router = require("express").Router();
 
+// endpoint: /
 router.get("/", (req, res) => {
     db.find()
         .then(posts => res.status(200).json(posts))
         .catch(err => res.status(500).json({error: "We couldn't get the posts for you. Internal error x_x"}))
+});
+
+router.post("/", (req, res) => {
+    db.find()
+        .then(posts => res.status(200).json(posts))
+        .catch(err => res.status(500).json({error: "We couldn't get the posts for you. Internal error x_x"}))
+});
+
+
+// endpoint: /:id
+router.get("/:id", (req, res) => {
+    
+})
+
+router.put("/:id", (req, res) => {
+    
+})
+
+router.delete("/:id", (req, res) => {
+    
+})
+
+
+// endpoint: /:id/comments
+router.get("/:id/comments", (req, res) => {
+
+})
+
+router.post("/:id/comments", (req, res) => {
+
 })
 
 module.exports = router;
