b515d09317413db7f30c1096e28abbb56b2409a0
Devin Warrick <dev.warr@gmail.com>
2019-10-29 12:53:00 -0500

    posting a post is successful

--- routes/posts-router.js
+++ routes/posts-router.js
@@ -9,9 +9,23 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-    db.find()
-        .then(posts => res.status(200).json(posts))
-        .catch(err => res.status(500).json({error: "We couldn't get the posts for you. Internal error x_x"}))
+
+    if (!req.body.title || !req.body.contents) {
+        res.status(400).json({errorMessage:"Oops! You need both a 'title' and 'contents' for a new post!"});
+        return;
+    } 
+
+    const newPost = {
+        title: req.body.title,
+        contents: req.body.contents
+    };
+
+    db.insert(newPost)
+        .then(idObj => {
+            db.findById(idObj.id)
+                .then(post => res.status(201).json(post));
+        })
+        .catch(err => res.status(500).json({error: "We couldn't add the posts into the database. Internal error x_x"}))
 });
 
 
